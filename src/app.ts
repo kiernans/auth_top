6aa451f49e1c5945a5b15d5474076534e42494c9
Kiernan Soriano <nukabunny@gmail.com>
2025-07-22 16:42:43 -0700

    Added password hashing through bcryptjs

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import path from "node:path";
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import session from "express-session";
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
@@ -55,7 +55,8 @@ passport.use(
             if (!user) {
                 return done(null, false, { message: "Incorrect username" });
             }
-            if (user.password !== password) {
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
                 return done(null, false, { message: "Incorrect password" });
             }
 
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 import db from "../db/query";
+import bcrypt from "bcryptjs";
 
 async function getAllUsers(req: Request, res: Response) {
     const users = await db.getAllUsers();
-
+    console.log(users);
     res.render("index", { users: users, user: res.locals.currentUser });
 };
 
 async function createNewUserPost(req: Request, res: Response, next: NextFunction) {
     try {
-        await db.insertUser(req.body.username, req.body.password);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        await db.insertUser(req.body.username, hashedPassword);
         res.redirect("/");
     } catch (err) {
         return next(err);
