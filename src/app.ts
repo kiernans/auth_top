19f1a7dcd4a1df592b818ffea2374df87a4bd295
Kiernan Soriano <nukabunny@gmail.com>
2025-07-22 14:03:55 -0700

    Changed dotenv call

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,14 @@ import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import signupRouter from "./routes/signupRouter";
 import userController from "./controllers/userController";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app: Express = express();
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-require("dotenv").config();
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
